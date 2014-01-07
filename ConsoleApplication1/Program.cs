cc014edc4727ee0aab4e496dfdff5bb0f282731e
alin-rautoiu <alin.rautoiu@cti.pub.ro>
2014-01-07 15:19:47 +0200

    Conectat la baza de date
    Si scris template pentru procedura de stocare

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program : DBRead, DBWrite
+    {
+        static void Main(string[] args)
+        {
+            String connectionString = "server=ENGI-PC\\SQLEXPRESS;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=LogBD; " +
+                                       "connection timeout=30";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand selectAll = new SqlCommand("SELECT * FROM LogTable", connection);
+
+            connection.Open();
+            SqlDataReader reader = selectAll.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["LogTime"]);
+            }
+
+        }
+        
+
+        public LogTable Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(LogTable log)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DBWork/Program.cs
+++ DBWork/Program.cs
@@ -11,19 +11,7 @@ namespace DBWork
     {
         static void Main(string[] args)
         {
-            SqlConnection sqlConnection = new SqlConnection("user id=user;" +
-                                      "password=1234%asd;" + " Server=localhost\\SQLExpress;" + "Trusted_Connection=yes;"
-                                               + "Database=LogDB;");
-            SqlCommand selectAll = new SqlCommand("SELECT * FROM Name", sqlConnection);
-            sqlConnection.Open();
-            SqlDataReader dataReader = null;
-            dataReader = selectAll.ExecuteReader();
-            while (dataReader.Read())
-            {
-                Console.WriteLine(dataReader["Id"].ToString());
-                Console.WriteLine(dataReader["Name"].ToString());
-            }
-            Console.Read();
+            SqlConnection
         }
     }
 }
