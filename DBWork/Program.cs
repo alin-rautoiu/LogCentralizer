f4cca30c1dd36e3fc277d7c06286d40f351bc453
alin-rautoiu <alin.rautoiu@cti.pub.ro>
2014-01-07 13:52:06 +0200

    Adaugat fisier de configurare

--- DBWork/Program.cs
+++ DBWork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace DBWork
     {
         static void Main(string[] args)
         {
+            SqlConnection sqlConnection = new SqlConnection("user id=user;" +
+                                      "password=1234%asd;" + " Server=localhost\\SQLExpress;" + "Trusted_Connection=yes;"
+                                               + "Database=LogDB;");
+            SqlCommand selectAll = new SqlCommand("SELECT * FROM Name", sqlConnection);
+            sqlConnection.Open();
+            SqlDataReader dataReader = null;
+            dataReader = selectAll.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Console.WriteLine(dataReader["Id"].ToString());
+                Console.WriteLine(dataReader["Name"].ToString());
+            }
+            Console.Read();
         }
     }
 }
